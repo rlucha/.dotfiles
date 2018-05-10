1a5e410d9e1a09df35f94c240a3cb7cf31f87f2f
Roberto Lucha <roberto.lucha@uswitch.com>
2018-05-10 09:39:45 +0100

    Update zsh.sh

--- zsh.sh
+++ zsh.sh
@@ -1,3 +1,5 @@
+source ~/antigen/antigen.zsh
+
 antigen use oh-my-zsh
 
 antigen bundle git
@@ -7,3 +9,8 @@ antigen bundle lukechilds/zsh-nvm
 antigen bundle paulirish/git-open
 antigen bundle zsh-users/zsh-syntax-highlighting
 antigen bundle lukechilds/zsh-better-npm-completion
+antigen bundle rlucha/.dotfiles --loc=main.sh
+
+antigen apply
+autoload -U promptinit; promptinit
+prompt pure
